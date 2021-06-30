ab6bb0f8fd007421a6c8768feddb5ef48a9331f7
slayellow <home0085@naver.com>
2021-06-30 23:25:44 +0900

    [20210630] Learning Rate Modify

--- ModelManagement/model_management.py
+++ ModelManagement/model_management.py
@@ -100,7 +100,8 @@ class ModelManagement:
             self.train_validation_total_epoch = self.total_epoch
 
             # Learning Rate 조절하기
-            lr = self.learning_rate * (0.1 ** (epoch // 30))
+            # lr = self.learning_rate * (0.1 ** (epoch // 30))
+            lr = self.learning_rate
             for param_group in self.optimizer.param_groups:
                 param_group['lr'] = lr
 
